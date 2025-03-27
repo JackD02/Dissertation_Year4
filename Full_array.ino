ae27e4134ca62c1fa32e6561243b3a516e982cd2
Jack Donnelly <jackdonnelly02@gmail.com>
2025-03-27 05:00:48 +0000

    Update Full_array.ino

--- Full_array.ino
+++ Full_array.ino
@@ -1,96 +1,175 @@
+#include <NewPing.h>
 #include <SPI.h>
 #include <SD.h>
 
-#define CS_PIN 10 //chipset for sd
+// defines pins for respective rangefinders
+#define trigPin_left 6
+#define echoPin_left 7
 
-//range finder 1 (finder on left)
-#define TRIG1 6
-#define ECHO1 7
+#define trigPin_right 8
+#define echoPin_right 9
 
-//range finder 2 (finder on right)
-#define TRIG2 8
-#define ECHO2 9
+//applies the maximum distance rangefinders can see and what the max value difference between each rangefinder is
+//where it can be considered the middle of the bed
+#define max_distance 300
+#define threshold 10
 
-File myFile;
+//set up of rangefinders
+NewPing rf_right(trigPin_right, echoPin_right, max_distance);
+NewPing rf_left(trigPin_left, echoPin_left, max_distance);
+
+//defines pins for pressure sensors (analog)
+const int piezoPin = A0;
+const int piezoPin2 = A1;
+const int flexPin = A2;
+
+//setup for duration of test
+const unsigned long duration = 3600000; //1 hour = 3600000
+unsigned long startTime;
+
+//checking ping times for rangefinders in order to avoid
+//issues with each rangefinder hitting eachother and disrupting their values
+unsigned long lastPingTimeLeft = 0;
+unsigned long lastPingTimeRight = 0;
+const unsigned long pingInterval = 100; 
+
+float lastRightVal = 0;
+float lastLeftVal = 0;
+
+//Setup for saving data to Sd card
+int chipSelect = 10; //chip select pin for the MicroSD Card Adapter
+File file; // file object that is used to read and write data
+String filename = "FAT3.csv"; //name of file to be saved 
 
 void setup() {
   Serial.begin(9600);
+  //setting pins for left rf
+  pinMode(trigPin_left, OUTPUT);
+  pinMode(echoPin_left, INPUT);
 
-  if(!SD.begin(CS_PIN))
-  {
-    Serial.println("SD card failed");
+  //setting pins for right rf
+  pinMode(trigPin_right, OUTPUT);
+  pinMode(echoPin_right, INPUT);
+
+  //selecting cs pin for sd card
+  pinMode(chipSelect, OUTPUT);
+
+  if(!SD.begin(chipSelect)){ //checks for sd card
+    Serial.println("SD card not found");
     return;
   }
-  Serial.println("SD card Found");
+  Serial.println("SD card found!");
 
-  pinMode(TRIG1, OUTPUT);
-  pinMode(ECHO1, INPUT);
+  file = SD.open(filename, FILE_WRITE);
+  if(file){ //writes in initial column names for csv file
+    file.println("Timestamp (ms), Piezo Value Left, Piezo Value Right, Flex Value Middle, rf Distance left, rf Distance right, Label");
+    file.close();
+  } else{ 
+    Serial.println("Failed to open file"); 
+    }
 
-  pinMode(TRIG2, OUTPUT);
-  pinMode(ECHO2, INPUT);
+  startTime = millis();
 }
 
-long getDist(int trigPin, int echoPin){
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+String classifyState(int piezo_left, int piezo_right, int flex_middle, float rf_left, float rf_right){
+  // pre-processing to label data entry with useful label of what is happening
+  // useful for machine learning classification later
+  float diff = abs(rf_left - rf_right); //finds difference in distance between both rf values
 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  long dur = pulseIn(echoPin, HIGH);
-  return (dur / 2) * 0.0344; //converts to cm rather than m
-}
-
-String classifyState(int p1, int p2, int p3, int p4, int p5, long d1, long d2) {
-  int cumPress = p1 + p2 + p3 + p4 + p5;
-  long avgDist = (d1 + d2) / 2;
-
-  if (cumPress > 900 && avgDist < 20 && avgDist > 40) return "lying"; //VALUES SUBJECT TO CHANGE
-  if (cumPress > 900 && d1 < d2) return "lying_toward_left"; //VALUES SUBJECT TO CHANGE
-  if (cumPress > 900 && d1 > d2) return "lying_toward_right"; //VALUES SUBJECT TO CHANGE
-  if (cumPress < 900) return "empty"; //VALUES SUBJECT TO CHANGE
-  return "unkown"; //default
+  //middle of the bed (either distance values within threshold or flex sensor being pressed down on)
+  //flex sensor is more reliable (keep as OR gate)
+  if ((diff <= threshold && rf_left != 0 && rf_right != 0) || flex_middle > 764)
+   return "middle";
+  //left of bed (left distance less than right or left piezo > pressure than right one)
+  else if((rf_left < rf_right || (rf_right == 0 && rf_left != 0)) || piezo_left > piezo_right && piezo_right != 0 || piezo_left == 0) return "left";
+  //right of bed (right distance less than left or right piezo > pressure than left one)
+  else if((rf_right < rf_left || (rf_right != 0 && rf_left == 0)) || piezo_left < piezo_right && piezo_left != 0 || piezo_right == 0) return "right";
+  else return "N/A"; //default label
 }
 
 void loop() {
-  int pressure_left = analogRead(A0);
-  int pressure_right = analogRead(A1);
-  int pressure_centre = analogRead(A2);
-  int pressure_top = analogRead(A3);
-  int pressure_bottom = analogRead(A4);
+  unsigned long current = millis();
 
-  long dist_left = getDist(TRIG1, ECHO1);
-  delay(50);
-  long dist_right = getDist(TRIG2, ECHO2);
+  int sensorValue1 = analogRead(piezoPin);  // read sensor 1 LEFT
+  //scaling factor applied to the rightside sensor as it is on average more sensitive, for meaningful data to be gained 
+  //both sensors should have similar baselines
+  int sensorValue2 = analogRead(piezoPin2) / 3.5; // read sensor 2 RIGHT
+  int flexValue = analogRead(flexPin); // read flex sensor MIDDLE 
+  
+  if (millis() - startTime < duration){
+    unsigned long timestamp = millis() - startTime;
 
-  unsigned long timestamp = millis();
-  //gives label for what is happening (pre-processing)
-  String label = classifyState(pressure_left, pressure_right, pressure_centre, pressure_top, pressure_bottom, dist_left, dist_right);
+    //initialise distance values
+    float dist_right = 0;
+    float dist_left = 0;
 
-  myFile = SD.open("data.csv", FILE_WRITE);
+    // Trigger left rf if enough time has passed
+    if (current - lastPingTimeLeft >= pingInterval) {
+      lastPingTimeLeft = current; //updates variable
+      dist_left = rf_left.ping_cm(); //auto converts to cm
+    }
 
-  if (myFile) {
-    myFile.print(timestamp);
-    myFile.print(",");
-    myFile.print(pressure_left);
-    myFile.print(",");
-    myFile.print(pressure_right);
-    myFile.print(",");
-    myFile.print(pressure_centre);
-    myFile.print(",");
-    myFile.print(pressure_top);
-    myFile.print(",");
-    myFile.print(pressure_bottom);
-    myFile.print(",");
-    myFile.print(dist_left);
-    myFile.print(",");
-    myFile.print(dist_right);
-    myFile.print(",");
-    myFile.println(label);
-    myFile.close();
-    Serial.println("Data saved!");
+    // Trigger right rf if enough time has passed
+    if (current - lastPingTimeRight >= pingInterval) {
+      lastPingTimeRight = current; //updates variable
+      dist_right = rf_right.ping_cm(); //auto converts to cm
+    }
+
+    if(dist_left != 0) //if actual value given
+    {
+      lastLeftVal = dist_left; //update last left val variable
+    }
+    else dist_left = lastLeftVal; //else set current value to be same as last found val
+
+    if(dist_right != 0) //if actual value given
+    {
+      lastRightVal = dist_right; //update last right val variable 
+    }
+    else dist_right = lastRightVal; //else set current value to be same as last found val
+
+    //call method to classify data
+    String label = classifyState(sensorValue1, sensorValue2, flexValue, dist_left, dist_right);
+  
+  //FOR TESTING/DEBUGGING PURPOSES
+    Serial.println("Time");
+    Serial.print(timestamp);
+    Serial.print(" ms, Piezo Value Left: ");
+    Serial.println(sensorValue1);
+    Serial.print("Piezo Value Right: ");
+    Serial.println(sensorValue2);
+    Serial.print("Flex Value Middle: ");
+    Serial.println(flexValue);
+    
+    Serial.print("Distance left: ");
+    Serial.println(dist_left);
+    Serial.print("Distance right: ");
+    Serial.println(dist_right);
+    Serial.print("label: ");
+    Serial.print(label); 
+  ///
+    file = SD.open(filename, FILE_WRITE);
+    if(file){
+      // Saving data into SD card in correct csv format
+      file.print(timestamp);
+      file.print(", ");
+      file.print(sensorValue1);
+      file.print(", ");
+      file.print(sensorValue2);
+      file.print(", ");
+      file.print(flexValue);
+      file.print(", ");
+      file.print(dist_left);
+      file.print(", ");
+      file.print(dist_right);
+      file.print(", ");
+      file.println(label);
+      file.close();
+    } else {
+      Serial.println("Error opening file");
+    }
+    delay(10);
   } else {
-    Serial.println("Error opening file.");
+    Serial.println("Data Collection Complete");
+    while(true);
   }
 }
